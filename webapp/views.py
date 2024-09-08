a9954da4187278cbd96daa915280032c3a7e22db
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-09-08 22:24:34 +0600

    Registrations Things

--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+
+from .forms import CreateUserForm, LoginForm
 
 # Create your views here.
 
@@ -8,3 +9,18 @@ def home(request):
     # return HttpResponse("Hello World!!")
 
     return render(request, 'webapp/index.html')
+
+
+# - Registration
+
+def register(request):
+    form = CreateUserForm()
+    
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            # return redirect('')
+    context = {'form':form}
+    return render(request, 'webapp/register.html', context=context)
