dac92a9b5c0fd18b6ab3ebec3d7136a3ff9b36db
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-09-09 20:05:36 +0600

    Login Things.

--- webapp/views.py
+++ webapp/views.py
@@ -24,3 +24,26 @@ def register(request):
             # return redirect('')
     context = {'form':form}
     return render(request, 'webapp/register.html', context=context)
+
+
+
+# - Login
+
+def my_login(request):
+    form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+        
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                # return redirect('')
+    
+    context = {'form':form}
+    return render(request, 'webapp/my-login.html', context=context)
